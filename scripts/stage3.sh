606beeb784aea84ef0ac029c17bfc5c97c6e147d
Татьяна Ившина <tatiana.ivshina@playrix.com>
2023-05-10 23:49:25 +0300

    Edit satge 3

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -15,3 +15,6 @@ cat root/q2/* >> output/q2.csv
 cat root/q3/* >> output/q3.csv
 cat root/q4/* >> output/q4.csv
 cat root/q6/* >> output/q6.csv
+pip install pyproj
+pip install shapely
+spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar --packages org.apache.spark:spark-avro_2.12:3.0.3 scripts/bigdataproject.py
